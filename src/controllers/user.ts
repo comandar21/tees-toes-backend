9299cf71a99b51c0fc94aa681858271bea573404
Aniketh Panchidi <ani.p1995@gmail.com>
2021-10-07 19:23:32 +0530

    sendgrid template id changs

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -264,9 +264,11 @@ export const checkMahaFollow = async (req, res) => {
             const checkEmailCounter = await emailCounter()
             if (checkEmailCounter) {
               // Referral completes a task
-              await sendEmail(referrerData, 'd-27034206b677427eaecf6ddf3e8ff95a')
+              await sendEmail(referrerData, 'd-46b96f5b08c84f6a9dcfb66fa5b5d080')
               // For the referred person
-              await sendEmail(refereeData, 'd-ac15ca6f15024b5588224b78956af899')
+              await sendEmail(refereeData, 'd-d3d7ec51fcbb43cca6abed30d27b4db4')
+              // await sendEmail(referrerData, 'd-27034206b677427eaecf6ddf3e8ff95a')
+              // await sendEmail(refereeData, 'd-ac15ca6f15024b5588224b78956af899')
             }
             else {
               console.log('email limit exceded')
@@ -383,7 +385,8 @@ export const addEmailContractAddress = async (req, res) => {
             referral_link: checkUser.referral_link
           }
           //MAHA Referral welcome
-          await sendEmail(emailData, 'd-0a264b4c808b4ec2afd1d00fb68b55e5')
+          await sendEmail(emailData, 'd-4cc16d34aad04de6ab22a6138b8e10fe')
+          // await sendEmail(emailData, 'd-0a264b4c808b4ec2afd1d00fb68b55e5')
         }
         else {
           const emailData = {
@@ -391,7 +394,8 @@ export const addEmailContractAddress = async (req, res) => {
             to_email: checkUser.email,
           }
           // Non-referral SignUp
-          await sendEmail(emailData, 'd-a8fe643c0bbd42dcabc645fc5283ffb8')
+          await sendEmail(emailData, 'd-7740705c270f4814bfb717dd9ae8750e')
+          // await sendEmail(emailData, 'd-a8fe643c0bbd42dcabc645fc5283ffb8')
         }
       }
       // const emailMessage = 'Welcome to MahaDAO Referral Program'
